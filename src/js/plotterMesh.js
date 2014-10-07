17b3a0a7039566fda9fcb3138c3546d90ae0712d
Mark Durrant <mark@pebblecode.com>
2014-10-07 22:24:23 +0100

    add dummy data & mesh/square handling

---
+++ src/js/plotterMesh.js
@@ -0,0 +1,31 @@
+// input = grid of points of varying heights
+// parent array stores rows
+// child arrays store individual heights
+var meshInput = [
+  [ 0,  0,  0,  0, 0 ],
+  [ 0, 15, 20, 15, 0 ],
+  [ 0, 10, 25, 20, 0 ],
+  [ 0,  5, 10, 15, 0 ],
+  [ 0,  0,  0,  0, 0 ],
+];
+
+// get number of rows & columns in mesh
+var meshRows = meshInput.length,
+ meshColumns = meshInput[ 0 ].length;
+
+// mesh squares
+var meshSquares = [];
+
+// add height points to squares
+for ( var r = 0; r < meshRows - 1; r++ ) {
+  for ( var c = 0; c < meshColumns; c++ ) {
+    meshSquares.push({
+      topLeft: meshInput[ r ][ c ],
+      topRight: meshInput[ r ][ c + 1 ],
+      bottomLeft: meshInput[ r + 1 ][ c ],
+      bottomRight: meshInput[ r + 1 ][ c + 1  ]
+    });
+  }
+}
+
+console.log( meshSquares[ 6 ] );

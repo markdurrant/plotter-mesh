9737806868a5393ba3389a5ed8094f7d42e8b74d
Mark Durrant <mark@pebblecode.com>
2014-10-08 22:52:01 +0100

    start drawing section

--- src/js/plotterMesh.js
+++ src/js/plotterMesh.js
@@ -1,19 +1,17 @@
+// get canvas center
+var canvas = $( '#plotterMesh' ),
+    canvasCenter = new Point( canvas.width() / 2, canvas.height() / 2 );
+
 // distance between points in 2d mesh
-var baseLength     = 10,
+var baseLength = 10,
     diagonalLength = Math.sqrt( Math.pow( baseLength, 2 ) * 2 );
 
 // set a scale to make drawing easier
-var scale = 10;
+var scale = 20;
 
 // set cut styles
-var cutStyle = {
-  strokeColor: 'black',
-  strokeWidth: 2,
-};
-
-// set fold styles
-var foldStyle = cutStyle;
-    foldStyle.dashArray = [ 4, 4 ];
+var strokeWidth = 2,
+    dashArray = [ 4, 4 ];
 
 // a grid of points of varying heights
 // ● - ● - ●
@@ -68,6 +66,45 @@ function getAllSideLengths ( meshItem ) {
   };
 }
 
-for ( var i = 0; i < meshSquares.length; i++ ) {
-  console.log( getAllSideLengths( meshSquares[ i ] ) );
+// draw a line
+function drawLine ( from, to, color, dashed ) {
+  if ( dashed === true ) {
+    var dashVal = dashArray;
+  } else {
+    var dashVal = 0;
+  }
+
+  var line = new Path.Line({
+    from: from,
+    to: to,
+    strokeColor: color,
+    strokeWidth: strokeWidth,
+    dashArray: dashVal
+  });
+
+  return line;
 }
+
+// draw a mesh section
+function drawSection ( allSideLengths ) {
+  var section = new Group(),
+      AB = new Group(),
+      AC = new Group(),
+      BD = new Group(),
+      CD = new Group();
+
+
+  var B = new Point( canvasCenter - [ 0, allSideLengths.BC / 2 ] ),
+      C = new Point( canvasCenter + [ 0, allSideLengths.BC / 2 ] );
+
+  var BC = drawLine( B, C, 'red', true );
+
+  AB.addChildren([
+  ]);
+
+  section.addChildren( [ BC ] );
+  section.scale( scale );
+}
+
+// console.log( getAllSideLengths( meshSquares[ 0 ] ) );
+drawSection( getAllSideLengths( meshSquares[ 0 ] ) );
